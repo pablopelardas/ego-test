db0ad175e547c0686e307379046af4922f8764ae
pablo pelardas <pablo.pelardas@gmail.com>
2022-07-26 12:56:44 -0300

    initial configs: eslint, tailwind, fonts

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-disable quotes */
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: ["./src/**/*.{js,jsx,html}"],
+    theme: {
+        extend: {},
+        minWidth:{
+            'screen': '100vw'
+        }
+    },
+    plugins: [],
+};
