c3f6046d91d7defe24339cf3e8961cabbaaa6188
pablo pelardas <pablo.pelardas@gmail.com>
2022-07-28 20:26:14 -0300

    fix: Ver Modelo on hover

--- src/views/Home/components/ModelCard/ModelCard.jsx
+++ src/views/Home/components/ModelCard/ModelCard.jsx
@@ -2,25 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import formatPrice from '../../../../utils/formatPrice';
 import { useNavigate } from 'react-router-dom';
-import {useSelector} from 'react-redux';
-import {selectModel} from '../../../../app/slices/modelSlice.js';
 
 const ModelCard = ({model}) => {
-    const [selected, setSelected] = React.useState(false);
-    const storeModel = useSelector(selectModel);
+    const [hover, setHover] = React.useState(false);
     const navigate = useNavigate();   
 
-    React.useEffect(() => {
-        if (storeModel.id === model.id) {
-            setSelected(true);
-        }
-    }, [storeModel]);
     return (
-        <article onClick={() => navigate(`/detail/${model.id}`)} className='h-[182px] w-[268px] flex flex-col items-center cursor-pointer relative'>
-            <h2 className={`text-card_title font-semibold ${selected && 'text-selected_name'}`}>{model.name}</h2>
+        <article onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className='h-[182px] w-[268px] flex flex-col items-center relative'>
+            <h2 className={`text-card_title font-semibold ${hover && 'text-selected_name'}`}>{model.name}</h2>
             <p className='mt-2 text-card_price'>{model.year} | {formatPrice(model.price)}</p>
             <img className='object-cover h-48 w-96' src={model.thumbnail} alt='model'/>
-            {selected && <button className='w-[152px] h-[34px] absolute bottom-[-20px] bg-soft_black text-selected_button rounded-[4rem] hover:bg-white hover:border-stone-900 hover:border-2 hover:text-soft_black'>Ver Modelo</button>}
+            {hover && <button onClick={() => navigate(`/detail/${model.id}`)} className='w-[152px] h-[34px] absolute bottom-[-20px] bg-soft_black text-selected_button rounded-[4rem] hover:bg-white hover:border-stone-900 hover:border-2 hover:text-soft_black'>Ver Modelo</button>}
         </article>
     );
 };
